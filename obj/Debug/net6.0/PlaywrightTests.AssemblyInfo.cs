de26a2e16ccc89f8c5e94024b3589463de225746
Santosh Addagatla <saddagatla@silverchair.com>
2024-03-05 18:50:31 -0500

    removed net8.0

--- obj/Debug/net6.0/PlaywrightTests.AssemblyInfo.cs
+++ obj/Debug/net6.0/PlaywrightTests.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a6cf0dd564e37913aefff51eeebbecf6ab743c3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlaywrightTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
